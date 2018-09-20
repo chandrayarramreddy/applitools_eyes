be62361a4eaeeb35e8a0d4ce5c861c0e789bdf15
chandrayarramreddy <chandra.yarramreddy@gmail.com>
2018-09-20 15:50:23 +0530

    code modified

--- Submit.java
+++ Submit.java
@@ -1,64 +1,57 @@
-
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import com.applitools.eyes.RectangleSize;
 import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.RectangleSize;
 
-public class Submit {  
-	
-	
-	WebDriver driver;
-	Eyes eyes;
-	String TestName;
-	
-	@Before
-	public void SetUp() {
-		
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\miracle\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		WebDriver browser=new ChromeDriver();
-		eyes=new Eyes();
-		//eyes.setApiKey("qKlqIJX7ro107C53Iq8jelF3gkFO3F3pCPQTXv7S43Rgc110");
+public class Image_compare_HelloWorld {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// Open a Chrome browser.
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\miracle\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		// Initialize the eyes SDK and set your private API key.
+		Eyes eyes = new Eyes();
+		//eyes.setApiKey("o56yRL2H10scuzrsd99t14Nwv106d16eh5oMQ8zZfFjz4U110");
 		eyes.setApiKey("2Dw5101Ql32LWT5U4CANwu9wCetWWsJqaypRXNNu66A1A110");
-	    driver=eyes.open(browser, "application",TestName,new RectangleSize(1000,600));
-	}
-	
-	@Rule
-    public TestWatcher watcher=new  TestWatcher() {
 		
-		@Override
+
+		try {
+
+			// Start the test and set the browser's viewport size to 800x600.
+			eyes.open(driver, "Hello World!", "My first Selenium Java test!", new RectangleSize(800, 600));
+
+			// Navigate the browser to the "hello world!" web-site.
+			// driver.get("https://applitools.com/helloworld");
+			driver.get("http://www.miraclesoft.com/");
+
+			// Visual checkpoint #1.
+			// eyes.checkWindow("Hello!");
+			eyes.checkWindow("Miracle");
+
+			// Click the "Click me!" button.
+			// driver.findElement(By.tagName("button")).click();
+			//driver.wait(10000);
 		
-		protected void starting(Description discription) {
-			TestName=discription.getDisplayName();
+			// Visual checkpoint #2.
+			eyes.checkWindow("Miracle");
+
+			// End the test.
+			eyes.close();
+
+		} finally {
+
+			// Close the browser.
+			driver.quit();
+
+			// If the test was aborted before eyes.close was called, ends the
+			// test as aborted.
+			eyes.abortIfNotClosed();
+		}
+
 	}
-};
-	
-	@After
-	public void TearDown() {
-		
-		driver.quit();
-	}
-   
-	
-	@Test
-	public void Login() throws InterruptedException {
-		
-		driver.navigate().to("https://www.facebook.com/");
-		eyes.checkWindow("login form");
-		driver.findElement(By.id("email")).sendKeys("002kumarharish");
-		driver.findElement(By.id("pass")).sendKeys("9866539941");
-		driver.findElement(By.id("loginbutton")).click();
-		eyes.checkWindow("login");
-		eyes.close();
-		driver.close();
-	}
-	
+
 }
